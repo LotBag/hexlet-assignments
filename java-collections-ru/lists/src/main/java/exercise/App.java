9be7b38b2c8c6ab185f5615c26e176965c9be3eb
@hexlet/cli <support@hexlet.io>
2023-10-16 20:33:47 +0700

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,8 +6,13 @@ import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String character, String word) {
+        character = character.toLowerCase();
+        word = word.toLowerCase();
         List<Character> chars = new ArrayList<>();
         List<Character> wordChars = new ArrayList<>();
+        int charsCounter = 0;
+        int tmpB;
+        int tmpA;
 
         for (var i = 0; i < character.length(); i++) {
             chars.add(character.charAt(i));
@@ -16,17 +21,29 @@ public class App {
         for (var i = 0; i < word.length(); i++) {
             wordChars.add(word.charAt(i));
         }
+        System.out.println(wordChars);
+        System.out.println(chars);
 
-        for (char wordChar : wordChars) {
-            for (char listChar : chars) {
-                if (wordChar == listChar) {
-                    wordChars.remove(wordChar);
-                    chars.remove(listChar);
+        for (char a : wordChars) {
+            for (char b : chars) {
+                if (a == b) {
+                    tmpA = wordChars.indexOf(a);
+                    System.out.println(tmpA);
+                    wordChars.set(tmpA, 'W');
+                    tmpB = chars.indexOf(b);
+                    System.out.println(tmpB);
+                    chars.set(tmpB, 'Q');
+                    charsCounter++;
+                    System.out.println(wordChars);
+                    System.out.println(chars);
+                    System.out.println(charsCounter);
+                    break;
+
                 }
             }
         }
-
-        return wordChars.isEmpty();
+        System.out.println(charsCounter);
+        return charsCounter >= word.length();
     }
 }
 //END
