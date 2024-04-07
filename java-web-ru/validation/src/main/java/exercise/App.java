93b9a9f645c09ff17c8f7a33ebd8e86eae61a6df
@hexlet/cli <support@hexlet.io>
2024-04-08 00:05:06 +0700

    submit java-web-ru/validation

---
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -0,0 +1,73 @@
+package exercise;
+
+import io.javalin.Javalin;
+import io.javalin.validation.ValidationException;
+import java.util.List;
+import exercise.model.Article;
+import exercise.dto.articles.ArticlesPage;
+import exercise.dto.articles.BuildArticlePage;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import io.javalin.rendering.template.JavalinJte;
+
+import exercise.repository.ArticleRepository;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/articles", ctx -> {
+            List<Article> articles = ArticleRepository.getEntities();
+            var page = new ArticlesPage(articles);
+            ctx.render("articles/index.jte", model("page", page));
+        });
+
+        // BEGIN
+        app.get("/articles/build" , ctx -> {
+            ctx.render("articles/build.jte");
+        });
+
+        app.post("/articles", ctx -> {
+            try {
+                String title = ctx.formParamAsClass("title", String.class)
+                        .check(value -> value.length() > 2,
+                                "Название не должно быть короче двух символов")
+                        .check(value -> ArticleRepository.findByTitle(value).isEmpty(),
+                                "Статья с таким названием уже существует")
+                        .get();
+
+
+                String content = ctx.formParamAsClass("content", String.class)
+                        .check(value -> value.length() > 10,
+                           "Статья должна быть не короче 10 символов")
+                        .get();
+
+                var articles = new Article(title, content);
+                ArticleRepository.save(articles);
+                ctx.redirect("/articles");
+            } catch (ValidationException e) {
+                String title = ctx.formParam("title");
+                String content = ctx.formParam("content");
+                var page = new BuildArticlePage(title, content, e.getErrors());
+                ctx.render("articles/build.jte", model("page", page));
+            }
+
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
