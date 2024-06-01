c526f2ec50065c953cda60bffc304b364103f6b5
@hexlet/cli <support@hexlet.io>
2024-06-02 00:41:30 +0700

    submit java-spring-ru/dto-to-entity-for-update

---
+++ java-spring-ru/dto-to-entity-for-update/src/main/java/exercise/dto/ProductUpdateDTO.java
@@ -0,0 +1,13 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Setter
+@Getter
+    public class ProductUpdateDTO {
+        private String title;
+        private int price;
+}
+// END
