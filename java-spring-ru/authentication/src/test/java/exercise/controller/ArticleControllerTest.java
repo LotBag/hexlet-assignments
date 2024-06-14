a7b8bce85c2b15eb058bbf5392ba9c553722ad93
@hexlet/cli <support@hexlet.io>
2024-06-15 01:05:11 +0700

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/test/java/exercise/controller/ArticleControllerTest.java
@@ -0,0 +1,14 @@
+
+package exercise.controller;
+
+
+import org.junit.jupiter.api.Test;
+
+class ArticleControllerTest {
+
+    @Test
+    void getArticle() throws Exception {
+        var a = "a";
+        a.equals("a");
+    }
+}
